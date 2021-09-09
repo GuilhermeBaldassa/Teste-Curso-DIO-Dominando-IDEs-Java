f16013c845a76eb230850801f18e3509aec9c8f5
Guilherme <guilherme.baldassa@gmail.com>
2021-09-09 09:19:47 -0300

    First commit

---
+++ src/br/com/DIO/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.DIO;
+
+import br.com.DIO.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		System.out.println(gato);
+		/*int a = 5;
+		int b = 2;
+		System.out.println("Hello World " + (a+b));*/
+
+	}
+
+}
